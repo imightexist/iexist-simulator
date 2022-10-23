bc5a8dc8eb9e2616eb6ee626b45cfb7110baee60
imightexist <94249457+imightexist@users.noreply.github.com>
2022-10-23 10:19:09 -0500

    Update fun.js

--- fun.js
+++ fun.js
@@ -8,7 +8,7 @@ let client = new discord.Client({
 client.login(token);
 client.on("messageCreate", function(message) {
   if (message.author.bot) return;
-  random = Math.floor(Math.random() * 4)
+  random = Math.floor(Math.random() * 7)
   if (message.content == "can you advertise github page for this repo uwu"){
     message.reply("https://github.com/imightexist/iexist-simulator")
   }else{
